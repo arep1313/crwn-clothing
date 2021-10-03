9408a5a8bd5f97fda698545fc82a6890b6f6cc27
arep1313 <arep18.2013@gmail.com>
2021-10-02 21:36:41 -0600

    added header component and links to home, shop page and (broken, not implemented yet:) contact. Shop price tag styles, still not fixed.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -22,8 +23,11 @@ function App() {
   // Exact is a true or false property. If true or not specified, it has to be exactly equal to the path. 
   //Without exact, if for example path='/', and we put '/hats' in the url, it will still render the homepage. 
   //what Switch does is that, the moment it finds a match, it will not render the rest. 
+
+  //Header is outside the switch, because this way, whatever path is on, it will always render the header. 
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage}/>
